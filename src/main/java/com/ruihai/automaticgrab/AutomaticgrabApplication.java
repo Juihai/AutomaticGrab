1e59e41444bec200c9ca53dddb1c357147b3b489
ruihai.shen <juihai@yeah.net>
2020-07-10 14:32:33 +0800

    fixes # - add

---
+++ src/main/java/com/ruihai/automaticgrab/AutomaticgrabApplication.java
@@ -0,0 +1,13 @@
+package com.ruihai.automaticgrab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticgrabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticgrabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ruihai/automaticgrab/AutomaticgrabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruihai.automaticgrab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticgrabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
